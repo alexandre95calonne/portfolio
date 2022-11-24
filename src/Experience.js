f18a9dbdefac62455ac3efa21f5564f95230dce3
alexandre95calonne <alexandre.calonne@epitech.digital>
2022-11-24 16:20:26 +0100

    portfolio final

--- src/Experience.js
+++ src/Experience.js
@@ -1,4 +1,4 @@
-import { Html ,ContactShadows ,PresentationControls ,Float, Environment ,useGLTF } from '@react-three/drei'
+import { Text ,Html ,ContactShadows ,PresentationControls ,Float, Environment ,useGLTF } from '@react-three/drei'
 
 export default function Experience()
 {
@@ -9,14 +9,39 @@ export default function Experience()
 
         <Environment preset='city' />
 
-        <color args={ [ '#695b5b' ]} attach="background" />
+        <color args={ [ '#262423' ]} attach="background" />
 
         <PresentationControls global rotation={ [0.13, 0.1, 0] } polar={[-0.4, 0.2]} azimuth={[-1, 0.75]} config={ {mass:2, tension:400}} snap={ {mass:4, tension:400}}>
             <Float rotationIntensity={ 0.4 }>
+                <rectAreaLight 
+                    width={2.5}
+                    height={1.65}
+                    intensity={65}
+                    color={'#DF402D'}
+                    rotation={[-0.1, Math.PI, 0]}
+                    position={[0, 0.55, -1.15]}
+                />
+
                 <primitive 
                     object={ computer.scene }
                     position-y={ - 1.2 }
-                />
+                    position-x={0.6}
+                    rotation-y={-0.2}
+                >
+
+                    <Html 
+                        transform
+                        wrapperClass='htmlScreen'
+                        distanceFactor={ 1.10 }
+                        position={ [0, 1.56, -1.4] }
+                        rotation-x={-0.256}
+                    >
+                        <iframe src='https://html.alexandre-calonne.fr/'/>
+                    </Html>
+
+                </primitive>
+
+               
             </Float>
         </PresentationControls>
 
